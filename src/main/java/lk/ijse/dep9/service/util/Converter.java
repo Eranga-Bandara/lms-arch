21ec9c273c84bc9f7cbc2cde7df390448ba561c1
Eranga Bandara <erangabandara94@gmail.com>
2022-11-24 11:12:06 +0530

    Use model mapper to handle entity dto conversion

--- src/main/java/lk/ijse/dep9/service/util/Converter.java
+++ src/main/java/lk/ijse/dep9/service/util/Converter.java
@@ -1,4 +1,27 @@
 package lk.ijse.dep9.service.util;
 
+import lk.ijse.dep9.dto.BookDTO;
+import lk.ijse.dep9.entity.Book;
+import org.modelmapper.ModelMapper;
+
 public class Converter {
+
+    private ModelMapper mapper = new ModelMapper();
+
+    public BookDTO fromBookEntity(Book bookEntity){
+//        return new BookDTO(bookEntity.getIsbn(),
+//                bookEntity.getTitle(),
+//                bookEntity.getAuthor(),
+//                bookEntity.getCopies());
+        return mapper.map(bookEntity, BookDTO.class);
+    }
+
+    public Book toBookEntity(BookDTO bookDTO){
+//        return new Book(bookDTO.getIsbn(),
+//                bookDTO.getTitle(),
+//                bookDTO.getAuthor(),
+//                bookDTO.getCopies());
+        return mapper.map(bookDTO, Book.class);
+    }
+
 }
