6dbd2a7c61444e8c36314a12491da37ea9415bba
Eranga Bandara <erangabandara94@gmail.com>
2022-11-24 16:27:09 +0530

    Add join query in placeNewIssueNote

--- src/main/java/lk/ijse/dep9/service/custom/impl/IssueServiceImpl.java
+++ src/main/java/lk/ijse/dep9/service/custom/impl/IssueServiceImpl.java
@@ -2,10 +2,7 @@ package lk.ijse.dep9.service.custom.impl;
 
 import lk.ijse.dep9.dao.DAOFactory;
 import lk.ijse.dep9.dao.DAOTypes;
-import lk.ijse.dep9.dao.custom.BookDAO;
-import lk.ijse.dep9.dao.custom.IssueItemDAO;
-import lk.ijse.dep9.dao.custom.IssueNoteDAO;
-import lk.ijse.dep9.dao.custom.MemberDAO;
+import lk.ijse.dep9.dao.custom.*;
 import lk.ijse.dep9.dto.IssueNoteDTO;
 import lk.ijse.dep9.service.custom.IssueService;
 import lk.ijse.dep9.service.exception.AlreadyIssuedException;
@@ -17,31 +14,34 @@ import lk.ijse.dep9.util.ConnectionUtil;
 
 public class IssueServiceImpl implements IssueService {
 
-    private IssueNoteDAO issueNoteDAO;
-    private IssueItemDAO issueItemDAO;
-    private MemberDAO memberDAO;
-    private BookDAO bookDAO;
-    private Converter converter;
+    private final IssueNoteDAO issueNoteDAO;
+    private final IssueItemDAO issueItemDAO;
+    private final MemberDAO memberDAO;
+    private final BookDAO bookDAO;
+    private final Converter converter;
+
+    private final QueryDAO queryDAO;
 
     public IssueServiceImpl(IssueNoteDTO issueNoteDTO) {
         issueNoteDAO = DAOFactory.getInstance().getDAO(ConnectionUtil.getConnection(), DAOTypes.ISSUE_NOTE);
         issueItemDAO = DAOFactory.getInstance().getDAO(ConnectionUtil.getConnection(), DAOTypes.ISSUE_ITEM);
         memberDAO = DAOFactory.getInstance().getDAO(ConnectionUtil.getConnection(), DAOTypes.MEMBER);
         bookDAO = DAOFactory.getInstance().getDAO(ConnectionUtil.getConnection(), DAOTypes.BOOK);
+        queryDAO = DAOFactory.getInstance().getDAO(ConnectionUtil.getConnection(), DAOTypes.QUERY);
         converter = new Converter();
-    }
-
-    public IssueServiceImpl() {
-
     }
 
     public void placeNewIssueNote(IssueNoteDTO issueNoteDTO) throws NotFoundException, NotAvailableException, LimitExceedException, AlreadyIssuedException {
 //        check member existance
         if(!memberDAO.existsById(issueNoteDTO.getMemberId())) throw new NotFoundException("Member does not exists");
 //        check book existance and availability
+        for(String isbn : issueNoteDTO.getBooks()){
+            int copies = queryDAO.getAvailableCopies(isbn).orElseThrow(() -> new NotFoundException("Book " + isbn + " does not exist"));
+            if(copies == 0) throw new NotAvailableException("Book " + isbn + " not available at the moment");
+        }
+//        check whether a book in issue note has been already issued to this member
 
 //        check how many books can be issued to this member maximum 3
-//        check whether a book in issue note has been already issued to this member
 
 
     }
