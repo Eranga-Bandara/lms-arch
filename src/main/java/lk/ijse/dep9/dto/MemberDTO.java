3ffc01aee04ccd73f9d1bcc9c4b417b657c87abc
Eranga Bandara <erangabandara94@gmail.com>
2022-11-28 16:51:01 +0530

    Validate member

--- src/main/java/lk/ijse/dep9/dto/MemberDTO.java
+++ src/main/java/lk/ijse/dep9/dto/MemberDTO.java
@@ -1,4 +1,6 @@
 package lk.ijse.dep9.dto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +11,18 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemberDTO implements Serializable {
+
     private String id;
+
+    @NotBlank(message = "Name can't be empty")
+    @Pattern(regexp = "[A-Za-z ]+", message = "Invalid name")
     private String name;
+
+    @NotBlank(message = "Address can't be empty")
+    @Pattern(regexp = "^[A-Za-z0-9| ,.:;#\\/\\\\-]+$", message = "Invalid address")
     private String address;
+
+    @NotBlank(message = "Contact number can't be empty")
+    @Pattern(regexp = "\\d{3}-\\d{7}", message = "Invalid contact number")
     private String contact;
 }
