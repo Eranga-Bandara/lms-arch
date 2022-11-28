62bed2e0095977e46443f3997add80ae3a99b247
Eranga Bandara <erangabandara94@gmail.com>
2022-11-28 18:01:03 +0530

    Validate return item

--- src/main/java/lk/ijse/dep9/dto/ReturnItemDTO.java
+++ src/main/java/lk/ijse/dep9/dto/ReturnItemDTO.java
@@ -1,5 +1,8 @@
 package lk.ijse.dep9.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +13,11 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ReturnItemDTO implements Serializable {
+
+    @NotNull(message = "Invalid issue note id")
     private Integer issueNoteId;
+
+    @NotBlank(message = "ISBN can't be empty")
+    @Pattern(regexp = "([0-9][0-9\\\\-]*[0-9])")
     private String isbn;
 }
